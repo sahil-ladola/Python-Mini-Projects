0d967d45efdbeba6fb9ee641b0f7245abefc951e
Sahil Ladola <110282930+sahil-ladola@users.noreply.github.com>
2024-02-03 12:59:10 +0530

    Update data_manager.py

--- Flight Deal Finder/data_manager.py
+++ Flight Deal Finder/data_manager.py
@@ -1,13 +1,13 @@
 import requests
 
-SHEETY_ENDPOINT = "https://api.sheety.co/c27c382024e5999321ff50d48c24758b/flightDeals/prices"
+SHEETY_PRICES_ENDPOINT = os.environ["SHEETY_PRICES_ENDPOINT"]
 
 class DataManager:
     def __init__(self):
         self.destination = {}
 
     def get_destination(self):
-        response = requests.get(url=SHEETY_ENDPOINT)
+        response = requests.get(url=SHEET_PRICES_ENDPOINT)
         data = response.json()
         self.destination = data["prices"]
         return self.destination
@@ -19,5 +19,5 @@ class DataManager:
                     "iataCode": city["iataCode"]
                 }
             }
-            response = requests.put(url=f"{SHEETY_ENDPOINT}/{city['id']}", json=new_data)
+            response = requests.put(url=f"{SHEET_PRICES_ENDPOINT}/{city['id']}", json=new_data)
             print(response.text)
